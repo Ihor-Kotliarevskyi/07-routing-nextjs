e084a721e058f22b061936276745e9401eba8696
Ihor Kotliarevskyi <jagor0707@gmail.com>
2026-01-10 17:20:54 +0200

    replacement files: loading.tsx, NoteDetails.client.tsx and NoteDetails.module.css

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,5 +1,5 @@
 import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
-import NoteDetailsClient from "../NoteDetails.client";
+import NoteDetailsClient from "./NoteDetails.client";
 import { fetchNoteById } from "@/lib/api";
 
 type Props = {
