fdfb93439ff625e666a8d92a131e5acb76502f20
Ihor Kotliarevskyi <jagor0707@gmail.com>
2026-01-11 12:46:52 +0200

    modified:   app/notes/page.tsx

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,9 +1,26 @@
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
 import NotesClient from "./Notes.client";
+import { fetchNotes } from "@/lib/api";
 
 async function Notes() {
+  
+  const searchText = "";
+  const currentPage = 1;
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["notes", searchText, currentPage],
+    queryFn: () => fetchNotes(searchText, currentPage),
+  });
 
   return (
-      <NotesClient/>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NotesClient />
+    </HydrationBoundary>
   );
 }
 
