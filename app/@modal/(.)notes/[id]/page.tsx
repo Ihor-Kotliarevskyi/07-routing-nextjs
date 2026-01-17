d4e584ec0da98f2919d88059e55650a42e4ad540
Ihor Kotliarevskyi <jagor0707@gmail.com>
2026-01-17 17:53:40 +0200

    replacement files

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -4,7 +4,7 @@ import {
   QueryClient,
 } from "@tanstack/react-query";
 import { fetchNoteById } from "@/lib/api";
-import NotePreviewClient from "./NoteProview.client";
+import NotePreviewClient from "./NotePreview.client";
 
 type Props = {
   params: Promise<{ id: string }>;
